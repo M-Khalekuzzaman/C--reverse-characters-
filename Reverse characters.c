62aa7594906c9d3c59ed16d9fbba2cf06a507e45
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-18 22:52:48 +0600

    Add files via upload

---
+++ Reverse characters.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char ch1,ch2,ch3;
+    printf("Enter your 3 characters are:");
+    scanf("%c%c%c",&ch1,&ch2,&ch3);
+
+printf("This 3 characters are : %c%c%c\n",ch1,ch2,ch3);
+printf("This reverse 3 characters are : %c%c%c\n",ch3,ch2,ch1);
+
+
+    getch();
+}
